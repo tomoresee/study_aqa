ab5bf0118f811fcec10cb6400ed5718ca4731e91
Kirill Kurochkin <75680082+tomoresee@users.noreply.github.com>
2026-07-10 12:37:13 +0600

    Revert "+test_login"

--- pages/main_page.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import Page
-
-
-class MainPage:
-    def __init__(self, page: Page) -> None:
-        self.login_button = page.get_by_test_id("nav-login")
-
-    def open_login_card(self):
-        self.login_button.click()
